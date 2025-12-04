b0125352e48865cd1319fe7a57346adbba19dcd2
jasonx <jason@mediapoint.com.au>
2025-12-04 15:49:24 +1100

    Fix environment for staging server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  isLocal: true,
-  url: 'http://localhost:4200/',
-  pusher_url: 'http://localhost',
-  api_url: 'http://localhost/api',
+  isLocal: false,
+  url: 'http://170.64.195.169/',
+  pusher_url: 'http://170.64.195.169',
+  api_url: 'http://170.64.195.169/api',
   pusher_key: 'stage-bsWnqxVWFP',
-  wsHost: 'localhost',
+  wsHost: '170.64.195.169',
   ewayKey: 'epk-4CC2DEF0-DDD1-4AAD-8DCF-E33B30DAFE66',
 };
 
